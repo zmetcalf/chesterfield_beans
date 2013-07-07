24057ff57af5a93e43e3b915f4843cc68bbf9cb3
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-07-07 18:01:14 -0400

    Inserted Default info

--- cms/admin.py
+++ cms/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from django.utils import timezone
 
 from cms.models import Content
 
 class ContentAdmin(admin.ModelAdmin):
     list_display = ('title', 'content_post_date')
+    
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'author', None) is None:
+            obj.author = request.user
+        if getattr(obj, 'content_post_date', None) is None:
+            obj.content_post_date = timezone.now()
+        obj.save()
 
 admin.site.register(Content, ContentAdmin)
--- cms/models.py
+++ cms/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Content(models.Model):
     title = models.CharField(max_length=200)
-    content_post_date = models.DateTimeField('date published')
+    content_post_date = models.DateTimeField('date published', null=True,
+                                                blank=True)
     content = models.TextField()
     summary = models.TextField()
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, null=True, blank=True)
     # TODO add tags, Haystack and Whoosh to handle search of content and tags
