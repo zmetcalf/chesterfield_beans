78688d9968cc3d219e6a67a8e3a3c574607c9f5d
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-07-09 22:00:44 -0400

    Reworked CMS Templates - added SEO

--- cms/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-from cms.views import show_content
-
-urlpatterns = patterns('',
-        url(r'^(?P<content_id>\d+)/$', show_content, name='content'),
-)
