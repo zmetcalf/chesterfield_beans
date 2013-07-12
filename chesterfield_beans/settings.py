2e2b63c29170fdb57d0e40731f7ad92e0d205fc4
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-07-12 17:49:31 -0400

    Reworked Templates added CSS

--- chesterfield_beans/settings.py
+++ chesterfield_beans/settings.py
@@ -71,6 +71,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), '../static').replace('\\', '/'),
 )
 
 # List of finder classes that know how to find static files in
@@ -107,7 +108,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), 'templates').replace('\\', '/'),
+    os.path.join(os.path.dirname(__file__), '../templates').replace('\\', '/'),
 )
 
 INSTALLED_APPS = (
