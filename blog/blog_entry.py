e7325d38c36369a71ad1eb9685cbb46acbae28fc
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-07-16 19:41:47 -0400

    Added ability to have multiple blogs

--- blog/blog_entry.py
+++ blog/blog_entry.py
@@ -3,6 +3,6 @@ from django.shortcuts import get_object_or_404
 from blog.models import BlogInfo, BlogEntry
 
 def get_blogroll(blog_id):
-    # TODO Insert logic for grabbing certain blog - ie blog_id
-    blogroll = BlogEntry.objects.all().order_by("-entry_post_date")[:10]
+    blog = get_object_or_404(BlogInfo, pk = blog_id)
+    blogroll = blog.blogentry_set.all().order_by("-entry_post_date")[:10]
     return {'blogroll': blogroll}
--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class BlogInfo(models.Model):
     title = models.CharField(max_length=200)
     tagline = models.CharField(max_length=200)
+    # Add blog id - http address safe
     
     def __unicode__(self):
         return self.title
