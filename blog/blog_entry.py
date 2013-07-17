445ca070b2653d7a7963c0ec347e1102f1bc4489
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-07-17 19:38:15 -0400

    Fixed some blog logic

--- blog/blog_entry.py
+++ blog/blog_entry.py
@@ -7,11 +7,14 @@ def get_blogroll(blog_id):
     blogroll = blog.blogentry_set.all().order_by("-entry_post_date")[:10]
     return {'blogroll': blogroll}
     
-def get_one_entry(blog_id, entry_id):
-    blog = get_object_or_404(BlogInfo, pk = blog_id)
-    entry = blog.blogentry_set.get(pk = entry_id)
+def get_one_entry(entry_id):
+    entry = get_object_or_404(BlogEntry, pk = entry_id)
     return {'entry': entry}
     
 def get_blog_info_title(blog_id):
     title = get_object_or_404(BlogInfo, pk = blog_id).title
     return {'title': title}
+    
+def get_recent(entries):
+    bloglist = BlogEntry.objects.all().order_by("-entry_post_date")[:entries]
+    return {'bloglist': bloglist}
--- site_management/views.py
+++ site_management/views.py
@@ -9,7 +9,7 @@ def show_cms(request, content_id):
     content = cms_content.get_content(content_id)
     site_title = get_object_or_404(SiteVariable, option_name__exact='Site Name')
     site_slogan = get_object_or_404(SiteVariable, option_name__exact='Site Slogan')
-    sidebar = cms_content.get_recent()
+    sidebar = blog_entry.get_recent(5)
     site_info = {'site_title': site_title, 'site_slogan': site_slogan,
                     'site_mode': 'cms', 'sidebar': 'left'}
     to_view =  Context(dict(list(content.items()) + list(site_info.items() + 
@@ -22,22 +22,21 @@ def show_blogroll(request, blog_id):
     title = blog_entry.get_blog_info_title(blog_id)
     site_title = get_object_or_404(SiteVariable, option_name__exact='Site Name')
     site_slogan = get_object_or_404(SiteVariable, option_name__exact='Site Slogan')
-    sidebar = cms_content.get_recent()
+    sidebar = blog_entry.get_recent(5)
     site_info = {'site_title': site_title, 'site_slogan': site_slogan,
                     'site_mode': 'blog', 'sidebar': 'left'}
     to_view = Context(dict(list(title.items()) + list(blogroll.items()) + list(site_info.items()) +
                         list(sidebar.items())))
     return render(request, 'blog/blogroll.html', to_view)
     
-def show_blogentry(request, blog_id, entry_id):
-    entry = blog_entry.get_one_entry(blog_id, entry_id)
-    title = blog_entry.get_blog_info_title(blog_id)
+def show_blog_entry(request, entry_id):
+    entry = blog_entry.get_one_entry(entry_id)
     site_title = get_object_or_404(SiteVariable, option_name__exact='Site Name')
     site_slogan = get_object_or_404(SiteVariable, option_name__exact='Site Slogan')
-    sidebar = cms_content.get_recent()
+    sidebar = blog_entry.get_recent(5)
     site_info = {'site_title': site_title, 'site_slogan': site_slogan,
                     'site_mode': 'blog', 'sidebar': 'left'}
-    to_view = Context(dict(list(title.items()) + list(entry.items()) + list(site_info.items()) +
+    to_view = Context(dict(list(entry.items()) + list(site_info.items()) +
                         list(sidebar.items())))
     return render(request, 'blog/entry.html', to_view)
     
