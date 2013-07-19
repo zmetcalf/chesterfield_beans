943df2790ded273c0259f8554d823d4f9f5073e9
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-07-19 14:12:52 -0400

    Added context_processors

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.template import Context
+from django.template import Context, RequestContext
 
 from blog import blog_entry
 from blog.models import BlogInfo, BlogEntry
@@ -10,14 +10,21 @@ def show_blogroll(request, blog_id):
     blogroll = blog.blogentry_set.all().order_by("-entry_post_date")[:10]
     title = blog_entry.get_blog_info_title(blog_id)
     sidebar = blog_entry.get_recent(5)
-    site_info = {'site_mode': 'blog', 'sidebar': 'left', 'blogroll': blogroll}
-    to_view = Context(dict(list(title.items()) + list(site_info.items()) +
+    site_info = {'sidebar': 'left', 'blogroll': blogroll}
+    to_view = Context(dict(list(title.items()) + list(site_info.items()) + 
                         list(sidebar.items())))
-    return render(request, 'blog/blogroll.html', to_view)
+    return render(request, 'blog/blogroll.html', to_view, context_instance = 
+                    RequestContext(request, processors=[context_processors]))
     
 def show_blog_entry(request, entry_id):
     entry = get_object_or_404(BlogEntry, pk = entry_id)
     sidebar = blog_entry.get_recent(5)
-    site_info = {'site_mode': 'blog', 'sidebar': 'left', 'entry': entry}
+    site_info = {'sidebar': 'left', 'entry': entry}
     to_view = Context(dict(list(site_info.items()) + list(sidebar.items())))
-    return render(request, 'blog/entry.html', to_view)
+    return render(request, 'blog/entry.html', to_view, context_instance = 
+                    RequestContext(request, processors=[context_processors]))
+    
+def context_processors(request):
+    return {
+        'site_mode': 'blog'
+    }
--- cms/views.py
+++ cms/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from django.template import Context
+from django.template import Context, RequestContext
 
 from blog import blog_entry
 from cms import cms_content
 from cms.models import Content
 
-
 def show_cms(request, content_id):
     content = content = get_object_or_404(Content, pk = content_id)
     sidebar = blog_entry.get_recent(5)
-    site_info = {'site_mode': 'cms', 'sidebar': 'left', 'content': content}
+    site_info = {'sidebar': 'left', 'content': content}
     to_view =  Context(dict(list(site_info.items() + list(sidebar.items()))))
-    return render(request, 'cms/content.html', to_view)
+    return render(request, 'cms/content.html', to_view, context_instance = 
+                    RequestContext(request, processors=[context_processors]))
+    
+def context_processors(request):
+    return {
+        'site_mode': 'cms'
+    }
