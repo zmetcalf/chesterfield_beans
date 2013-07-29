f4ff3bb8c3ec2f9cf0f8be38cf942d88ce968e24
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-07-29 19:12:46 -0400

    Start to write tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,16 +1,31 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.contrib.auth.models import User
 from django.test import TestCase
 
+from blog.models import BlogEntry, BlogInfo
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class BlogInfoTestCase(TestCase):
+    
+    def setUp(self):
+        BlogInfo.objects.create(title="Test Blog", tagline="This is just a test", url_slug="test-slug")
+        
+    def test_values_taken(self):
+        test_blog = BlogInfo.objects.get(pk=1)
+        self.assertEqual(test_blog.title, "Test Blog")
+        self.assertEqual(test_blog.tagline, "This is just a test")
+        self.assertEqual(test_blog.url_slug, "test-slug")
+        
+class BlogEntryTestCase(TestCase):
+    
+    def setUp(self):
+        blog_info_object = BlogInfo.objects.create(title="Test Blog", 
+            tagline="This is just a test", url_slug="test-slug")
+        user = User.objects.create_user('test', 'test@email.com', 'testword')
+        BlogEntry.objects.create(blog_info=blog_info_object, entry_title="Blog Entry Title",
+            entry_post_date='2011-09-01T13:20:30+03:00', entry="Here is the entry information",
+            author=user, seo_keywords="SEO,keywords,for,site", 
+            seo_description="Description for the site SEO Style", entry_slug="entry-test")
+        
+    def test_values_taken(self):
+        test_entry = BlogEntry.objects.get(pk=1)
+        self.assertEqual(test_entry.entry_title, "Blog Entry Title")
+        
